01c3a992c3cf5889614edb8a56de7c6fe12f6a40
Matheus Penchel <matheus.penchel@gmail.com>
2021-01-22 10:54:44 -0300

    Finished the Task Manager

--- lectures/cookbook-day-1/router.rb
+++ lectures/cookbook-day-1/router.rb
@@ -1,6 +1,7 @@
 class Router
 
-  def initialize()
+  def initialize(controller)
+    @controller = controller
   end
 
   def run
@@ -16,9 +17,9 @@ class Router
       elsif choice == 2
         @controller.create_a_task
       elsif choice == 3
-
+        @controller.mark_task_as_completed
       else
-
+        puts "Invalid operation.... Please choose a valid one"
       end
     end
   end
--- lectures/cookbook-day-1/test_scenario.rb
+++ lectures/cookbook-day-1/test_scenario.rb
@@ -2,16 +2,10 @@ require_relative 'repository'
 require_relative 'task'
 require_relative 'view'
 require_relative 'controller'
+require_relative 'router'
 
 repository = Repository.new
 view       = View.new
 controller = Controller.new(repository, view)
-
-controller.create_a_task
-controller.create_a_task
-
-controller.list_all_tasks
-
-controller.mark_task_as_completed
-
-controller.list_all_tasks
+router     = Router.new(controller)
+router.run
